a87c379e181a5c09456ff1e87f2ddc6e3231b968
Mitchap <mitchbarcenilla4@gmail.com>
2024-11-10 22:24:51 +0800

    Add project files.

---
+++ StudentPortal/Models/AddStudentViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentPortal.Models
+{
+    public class AddStudentViewModel
+    {
+        required
+        public string Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
